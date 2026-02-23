069eb250841f4bc05c1320f14c67a26785e27d46
Swethamadda0710 <maddaswetha9@gmail.com>
2026-02-23 12:47:37 +0530

    fourth commit

--- src/test/java/com/aditya/csejenkins1/CseaddTest.java
+++ src/test/java/com/aditya/csejenkins1/CseaddTest.java
@@ -11,9 +11,9 @@ void csetest() {
 	int result = c.add(2, 3);
 	assertEquals(5,result);
 }
-@Test
-void csetest1() {
-	int result = c.mul(2, 3);
-	assertEquals(6,result);
-}
+//@Test
+//void csetest1() {
+//	int result = c.mul(2, 3);
+//	assertEquals(6,result);
+//}
 }
